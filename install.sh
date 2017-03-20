f94fb83906c20d2e638cfdd0a930942d3fd4083d
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-03-20 09:49:06 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,6 +26,9 @@ sudo dpkg -a --configure
 sudo apt-get dist-upgrade
 sudo dpkg --configure -a
 sudo sudo apt-get dist-upgrade
+sudo apt-get install -y tor
+sudo apt-get install -y openvpn
+sudo apt-get install network-manager-openvpn
 sudo apt-get install lua50
 sudo apt-get install lua5.1
 sudo apt-get install lua5.2
@@ -33,6 +36,8 @@ sudo apt-get install lua5.3
 wget "https://valtman.name/files/telegram-cli-1222"
 sudo apt-get install libreadline6 libreadline-dev libreadline-dev libreadline6-dev libconfig-dev libssl-dev tmux lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo service tor start
+sudo service openvpn start
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
@@ -40,6 +45,9 @@ sudo ppa-purge
 sudo service redis-server restart
 chmod 777 telegram-cli-1222
 chmod 777 anticrash.sh
+export http_proxy = "socks5://127.0.0.1:9050"
+export https_proxy = "socks5://127.0.0.1:9050"
+export ftp_proxy = "socks5://127.0.0.1:9050"
    echo -e "\033[38;5;600m"
    echo -e " :::::::::::  ::::::::     ::::    ::::  :::::::::: ::::    ::::  :::::::::  :::::::::: ::::::::: "
    echo -e "     :+:     :+:    :+:    +:+:+: :+:+:+ :+:        +:+:+: :+:+:+ :+:    :+: :+:        :+:    :+:"
